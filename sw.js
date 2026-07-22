e86e8abdb1681d41b87a60ecf31fab1029a88a5d
Tyler <troche8@gatech.edu>
2026-07-21 20:12:11 -0400

    Changed Android Logo + Name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'field-notes-v1';
+const CACHE_NAME = 'todo-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
